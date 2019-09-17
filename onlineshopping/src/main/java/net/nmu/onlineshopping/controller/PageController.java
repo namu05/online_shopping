f9cc7654670d907177fdf0b52d7eadfc00b3ff2c
namu05 <naman.katuva@gmail.com>
2019-09-18 01:22:24 +0530

    Creating other pages and solvig active menu problem!!!

--- onlineshopping/src/main/java/net/nmu/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/nmu/onlineshopping/controller/PageController.java
@@ -9,10 +9,29 @@ public class PageController {
 
 	
 	@RequestMapping(value= {"/","/home","/index"})
-	public ModelAndView index()
+	public ModelAndView home()
 	{
 		ModelAndView mv=new ModelAndView ("page");
-		mv.addObject("greeting","Welcome Sahil and Sakshi");
+		mv.addObject("title","Home");
+		mv.addObject("userClickHome","true");
+		return mv;
+	}
+	
+	@RequestMapping(value= {"/contact"})
+	public ModelAndView contact()
+	{
+		ModelAndView mv=new ModelAndView ("page");
+		mv.addObject("title","Contact Us");
+		mv.addObject("userClickContact","true");
+		return mv;
+	}
+	
+	@RequestMapping(value= {"/about"})
+	public ModelAndView about()
+	{
+		ModelAndView mv=new ModelAndView ("page");
+		mv.addObject("title","About Us");
+		mv.addObject("userClickAbout","true");
 		return mv;
 	}
 }
